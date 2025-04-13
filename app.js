1b9afe95e6a2207ecda7b24c27bb47a75ce23546
gerardosdl <gerardo.sdl.os@gmail.com>
2025-04-12 21:32:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const communityHeading = document.querySelector('h6.text-center');
 
 communityHeading.style.fontSize = '2em';
 communityHeading.style.color = 'purple';
+
+
+document.addEventListener('click', function() {
+  const mainHeading = document.querySelector('h1');
+
+  mainHeading.style.backgroundColor = 'red';
+})
